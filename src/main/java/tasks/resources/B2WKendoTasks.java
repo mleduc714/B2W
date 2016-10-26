1e898a893a3650541b02b84d34c339a73d43a6d1
mleduc <mleduc@MLEDUC-DT.hq.bid2win.com>
2016-10-26 13:06:03 -0400

    Commite

--- src/main/java/tasks/resources/B2WKendoTasks.java
+++ src/main/java/tasks/resources/B2WKendoTasks.java
@@ -1,17 +1,15 @@
 package tasks.resources;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import appobjects.scheduler.B2WScheduleAssignments;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 import appobjects.resources.B2WEquipment;
 import appobjects.resources.KendoUI;
+import appobjects.scheduler.B2WScheduleAssignments;
 import tasks.BrowserUtils;
 import tasks.WebElementUtils;
 import tasks.util.TaskUtils;
