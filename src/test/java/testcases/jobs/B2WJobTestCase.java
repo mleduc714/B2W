feb35048c312a6b0a9dfb78b3ea1227aa2bb2411
mleduc <mleduc@MLEDUC-DT.hq.bid2win.com>
2016-10-28 07:03:31 -0400

    Add Testcase

--- src/test/java/testcases/jobs/B2WJobTestCase.java
+++ src/test/java/testcases/jobs/B2WJobTestCase.java
@@ -2,6 +2,67 @@ package testcases.jobs;
 
 import com.b2w.test.B2WTestCase;
 
+import tasks.jobs.B2WJobsTasks;
+
 public class B2WJobTestCase extends B2WTestCase {
+	
+	private B2WJobsTasks b2wJobs = new B2WJobsTasks();
+
+	@Override
+	public void testSetUp() throws Throwable {
+		// TODO Auto-generated method stub
+		super.testSetUp();
+	}
+
+	@Override
+	public void testMain() throws Throwable {
+		// TODO Auto-generated method stub
+		
+		//Navigate to the Jobs area and create a new Job
+				//a.Populate the General Information 
+		//Create a Job Site
+		        // populate all info
+		// add materials
+		       // add Crushed Gravel CRGRVL02
+		//add a vendor
+		      // add northern steel
+		// add job production account
+		     // enter info
+		// create new t&m work item
+		// open the job
+		// delete the job
+				
+		super.testMain();
+	}
+
+	@Override
+	public void testTearDown() throws Throwable {
+		// TODO Auto-generated method stub
+		super.testTearDown();
+	}
+
+	@Override
+	public String getCategory() {
+		// TODO Auto-generated method stub
+		return super.getCategory();
+	}
+
+	@Override
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return super.getAuthor();
+	}
+
+	@Override
+	public String getDataPath() {
+		// TODO Auto-generated method stub
+		return super.getDataPath();
+	}
+
+	@Override
+	public boolean isSupported() {
+		// TODO Auto-generated method stub
+		return super.isSupported();
+	}
 
 }
