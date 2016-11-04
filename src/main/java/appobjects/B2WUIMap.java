43fea0735eb321be1e6c2c64f683edd1f57dd91a
atrostyanko <Alexandr.Trostyanko@artezio.com>
2016-11-04 17:44:12 +0300

    AUT-55 Refactoring Schedule View creation

--- src/main/java/appobjects/B2WUIMap.java
+++ src/main/java/appobjects/B2WUIMap.java
@@ -122,9 +122,12 @@ public class B2WUIMap {
 	// Schedules
 	public static final String b2w_schedules_panel = ".k-pane.k-scrollable";
 	public static final String b2w_schedules_createscheduleviewbtn = "span.btn-add";
+	public static final String b2w_schedules_editscheduleviewbtn = "span.btn-edit";
 	public static final String b2w_schedules_headline = "div.headline";
 	public static final String b2w_schedules_name = "input.k-textbox[name='name']";
 	public static final String b2w_schedules_notes = "textarea[name='notes']";
+	public static final String b2w_schedules_scheduleformat = "input[name='scheduleFormat']";
+	public static final String b2w_schedules_input = "input";
 	public static final String b2w_schedules_groupitemsby = "select.grouping-dropdown[data-bind *= 'scheduledGrouping1DataSource']";
 	public static final String b2w_schedules_secondarygrouping = "select.grouping-dropdown[data-bind *= 'scheduledGrouping2DataSource']";
 	public static final String b2w_schedules_savebtn = ".btn-form.large.save";
@@ -133,7 +136,7 @@ public class B2WUIMap {
 	public static final String b2w_schedules_filterlabel = "label";
 	public static final String b2w_schedules_deletebtn = ".btn-delete";
 	public static final String b2w_schedules_deletepopupwindow = ".k-widget.k-window";
-	public static final String getB2w_schedules_popupwindow_yesbtn = ".btn-form.dialog-button.large.confirm";
+	public static final String b2w_schedules_popupwindow_yesbtn = ".btn-form.dialog-button.large.confirm";
 
 	// page filter
 	public static final String b2w_page_filter_letterA = "a#PageContent_FilterControl_AButton";
@@ -771,7 +774,8 @@ public class B2WUIMap {
 	public static final String b2w_schedulecreationsection = "b2w-create-action";
 	public static final String b2w_scheduleassignmentwindow = ".k-widget.k-window.close-button";
 	public static final String b2w_schedulefieldslabel = "label";
-	public static final String b2w_scheduleaddtoschedulebtn = ".btn-form.large";
+	//public static final String b2w_scheduleaddtoschedulebtn = ".btn-form.large";
+	public static final String b2w_scheduleaddtoschedulebtn = ".Button.Button--large.Button--margin-left";
 	public static final String b2w_schedulecontrolpanel = ".control-panel";
 	public static final String b2w_scheduleselectcrewbtn = ".btn-form.large";
 	public static final String b2w_schedulebuttoncontainer = ".button-container";
--- src/main/java/appobjects/setup/B2WSchedules.java
+++ src/main/java/appobjects/setup/B2WSchedules.java
@@ -6,9 +6,16 @@ import org.openqa.selenium.By;
 public class B2WSchedules {
     public static By rightPanel() { return By.cssSelector(B2WUIMap.b2w_schedules_panel); }
     public static By createScheduleViewBtn() { return By.cssSelector(B2WUIMap.b2w_schedules_createscheduleviewbtn); }
+    public static By editScheduleViewBtn() { return By.cssSelector(B2WUIMap.b2w_schedules_editscheduleviewbtn); }
     public static By headline() { return By.cssSelector(B2WUIMap.b2w_schedules_headline); }
     public static By nameField() { return By.cssSelector(B2WUIMap.b2w_schedules_name); }
     public static By notes() { return By.cssSelector(B2WUIMap.b2w_schedules_notes); }
+    public static By scheduleFormat() { return By.cssSelector(B2WUIMap.b2w_schedules_scheduleformat); }
+    public static By scheduleFormatItem(String sValue) { return By.cssSelector(B2WUIMap.b2w_schedules_scheduleformat + "[value='" + sValue + "']"); }
+    public static By scheduleCheckBox(String sValue) { return By.cssSelector(B2WUIMap.b2w_schedules_input + "[name='" + sValue + "']"); }
+    public static By scheduleResourceBtn(String sValue) { return By.cssSelector(B2WUIMap.b2w_schedules_input + "[value='" + sValue + "']"); }
+
+    //ToDo Remove All unnecessary methods
     public static By groupItemsBy() { return By.cssSelector(B2WUIMap.b2w_schedules_groupitemsby); }
     public static By secondaryGrouping() { return By.cssSelector(B2WUIMap.b2w_schedules_secondarygrouping); }
     public static By saveBtn() { return By.cssSelector(B2WUIMap.b2w_schedules_savebtn); }
@@ -17,5 +24,5 @@ public class B2WSchedules {
     public static By filterItemLabel() { return By.cssSelector(B2WUIMap.b2w_schedules_filterlabel); }
     public static By deleteBtn() { return By.cssSelector(B2WUIMap.b2w_schedules_deletebtn); }
     public static By deletePopUpWindow() { return By.cssSelector(B2WUIMap.b2w_schedules_deletepopupwindow); }
-    public static By yesBtnOnPopupWindow() { return By.cssSelector(B2WUIMap.getB2w_schedules_popupwindow_yesbtn); }
+    public static By yesBtnOnPopupWindow() { return By.cssSelector(B2WUIMap.b2w_schedules_popupwindow_yesbtn); }
 }
