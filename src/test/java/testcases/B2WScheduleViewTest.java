5a4cb4a9051a7d77de1949a8744bd75dbddf589e
idatsko <idatsko@IDAT-DT.hq.bid2win.com>
2016-11-18 09:40:44 -0500

    schedule test change

--- src/test/java/testcases/B2WScheduleViewTest.java
+++ src/test/java/testcases/B2WScheduleViewTest.java
@@ -68,358 +68,358 @@ public class B2WScheduleViewTest extends B2WTestCase{
 		Timer timer = new Timer();
 		B2WSchedulerTasks schedulerTasks = new B2WSchedulerTasks();
 		
-		timer.start();
 		b2wNav.openSchedule();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
         linkTable.put("Open Schedule", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnCrews();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
         linkTable.put("Crews", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnCrewsByAssignmentLocation();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
         linkTable.put("Crews by Assignment Location", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnCrewsByWorkSubtype();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
         linkTable.put("Crews by Work Subtype", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnCrewsByWorkType();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
         linkTable.put("Crews by Work Type", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnCrewsByWorkTypeAndWorkSubtype();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
         linkTable.put("Crews by Work Type & Work Subtype", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEmployeeSchedule();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Employee Schedule", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEmployeesByAssignmentLocation();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Employees by Assignment Location", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEmployeesByBusinessUnit();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Employees by Business Unit", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEmployeesByLaborType();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Employees by Labor Type", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByCategory();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Category", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByCategoryAndLocation();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Category & Location", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByCategoryAndType();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Category & Type", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByLocation();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Location", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByLocationAndCategory();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Location & Category", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByLocationAndType();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Location & Type", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByType();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Type", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByTypeAndCategory();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Type & Category", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentGroupedByTypeAndLocation();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Grouped by Type & Location", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule1();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 1", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule2();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 2", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule3();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 3", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule4();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 4", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule5();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 5", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule6();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 6", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule7();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 7", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule8();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 8", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnEquipmentSchedule9();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Equipment Schedule 9", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByEquipmentAtRt93Exit18();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Equipment At RT93 Exit 18 ", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByEquipmentDownForMaintenance();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Equipment Down for Maintenance", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByEquipmentScheduledForMaintenance();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Equipment Scheduled for Maintenance", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByEquipmentThatMovesOtherEquipment();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered By Equipment That Moves other Equipment", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByEquipmentThatRequiresMoves();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Equipment That Requires Moves", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByEquipmentWithGPS();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Equipment With GPS", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByEquipmentWithMemoEvents();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Equipment With Memo Events", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByOwnedEquipmentAtEqYardOrHQ();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Owned Equipment at Eq. Yard or HQ", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByRentedAndSubcontractedEquipment();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Rented & Subcontracted Equipment", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredByRentedCranes();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Rented Cranes", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredBySelfMobileEquipment();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Self Mobile Equipment", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredBySitework();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered by Sitework", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnFilteredBySubcontractedTriAxles();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Filtered By Subcontracted Tri-Axles", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnJobSites();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Job Sites", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnJobSitesPlaces();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Job Sites - Places", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnMortsInactiveEquipment();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Mort's <Inactive> Equipment", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnMortsActiveEquipment();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Mort's Active Equipment", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnNortheastDivisionAndPaversAndGraders();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Northeast Division & Pavers & 'Graders'", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnNorthwestDivisionPaversGraders();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Northwest Division Pavers-Graders", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnPlaces();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Places", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnSouthernDivisionSiteworkSpecial();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Southern Division Sitework \"Special\"", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnSouthwestDivisionPaversGraders();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Southwest Division Pavers--Graders", timer.getTotalTime());
 		
 		scheduleView.openScheduleViewList();
-		timer.start();
 		scheduleView.clickOnRandomCharacters();
+		timer.start();
 		schedulerTasks.waitForSchedulePageNoBusy();
 		timer.end();
 		linkTable.put("Z!\"#$%&'()*+-./:;<=>?@[\\]^_`{|}~“”‘’!\"#$%&'()*+-./:;<=>?@[\\]^_`{|}~“”‘’", timer.getTotalTime());
