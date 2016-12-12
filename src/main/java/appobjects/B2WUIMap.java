c0e5222e2cd959a380f9da63ece0ee8ce853eb8e
mleduc <mleduc@MLED-DT.hq.bid2win.com>
2016-12-12 15:34:29 -0500

    Revert

--- src/main/java/appobjects/B2WUIMap.java
+++ src/main/java/appobjects/B2WUIMap.java
@@ -852,7 +852,7 @@ public class B2WUIMap {
 	// work orders
 	public static final String b2w_maintainworkorderslistview = "div#work_order_list_view";
 	public static final String b2w_maintainworkorderdisplaystatus = "td.subhead.right";
-	public static final String b2w_maintainworkordersview = "div#work_order_edit_view";
+	public static final String b2w_maintainworkordersview = "div#work_order_create_view";
 	public static final String b2w_maintainworkorderadditembutton = "a.btn-orig.add-item";
 	public static final String b2w_maintainworkorderadditemview = "div.create-new-request";
 	public static final String b2w_maintainworkorderselectitemsview = "div.select-action.select-request";
