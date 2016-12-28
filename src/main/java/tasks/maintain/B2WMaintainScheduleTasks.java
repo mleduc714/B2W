d731b621809e700965960552ef2a728ba9512e4f
mleduc <mleduc@MLED-DT.hq.bid2win.com>
2016-12-28 13:19:26 -0500

    Update test getting date

--- src/main/java/tasks/maintain/B2WMaintainScheduleTasks.java
+++ src/main/java/tasks/maintain/B2WMaintainScheduleTasks.java
@@ -701,12 +701,17 @@ public class B2WMaintainScheduleTasks extends B2WKendoTasks {
 		try {
 			SimpleDateFormat calendardate = new SimpleDateFormat("EEE MM/dd/yyyy");
 			WebElement el = WebElementUtils.findElement(B2WMaintain.getB2WScheduleFormatDate());
+			
 			if (el != null) {
 				sDate = el.getText();
 				int iIndex = sDate.indexOf("-");
 				if (iIndex != -1) {
 					sDate = sDate.substring(0, iIndex).trim();
 				}
+				date = calendardate.parse(sDate);
+			}else{
+				el = WebElementUtils.findElement(B2WMaintain.getB2WScheduleSmallFormatDate());
+				sDate = el.getText();
 				date = calendardate.parse(sDate);
 			}
 		} catch (Exception e) {
