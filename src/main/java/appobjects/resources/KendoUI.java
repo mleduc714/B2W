89a640035b87221b3353fa92422a97d555e61786
mleduc <mleduc@MLED-DT.hq.bid2win.com>
2017-01-23 07:28:29 -0500

    Strings for tags

--- src/main/java/appobjects/resources/KendoUI.java
+++ src/main/java/appobjects/resources/KendoUI.java
@@ -9,135 +9,178 @@ public abstract class KendoUI {
 	public static By getKendoDescription() {
 		return By.cssSelector(B2WUIMap.b2w_kendoinputtexbox);
 	}
+
 	public static By getKendoDropDown() {
 		return By.cssSelector(B2WUIMap.b2w_kendoinput);
 	}
+
 	public static By getKendoDropDownItem() {
 		return By.cssSelector(B2WUIMap.b2w_kendodropdownitem);
 	}
+
 	public static By getKendoNameValuePair() {
 		return By.cssSelector(B2WUIMap.b2w_kendonamevaluepair);
 	}
+
 	public static By getKendoLists() {
 		return By.cssSelector(B2WUIMap.b2w_kendolistofitems);
 	}
+
 	public static By getKendoFilterByDD() {
 		return By.cssSelector(B2WUIMap.b2w_kendofilterbydd);
 	}
+
 	public static By getKendoGridContent() {
 		return By.cssSelector(B2WUIMap.b2w_kendogridcontent);
 	}
+
 	public static By getKendoItems() {
 		return By.cssSelector(B2WUIMap.b2w_kendoitems);
 	}
+
 	public static By getKendoHeadersFromView() {
 		return By.cssSelector(B2WUIMap.b2w_kendoheaders);
 	}
+
 	public static By getKendoPageLoading() {
 		return By.cssSelector(B2WUIMap.b2w_kendoloadpage);
 	}
+
 	public static By getKendoInputTextBox() {
 		return By.cssSelector(B2WUIMap.b2w_kendoinputtexbox);
 	}
 	public static By getKendoLabel() {
 		return By.cssSelector(B2WUIMap.b2w_kendolabel);
 	}
+
 	public static By getKendoFormattedValue() {
 		return By.cssSelector(B2WUIMap.b2w_kendoformatedvalue);
 	}
+
 	public static By getKendoDropDownForTMTab() {
 		return By.cssSelector(B2WUIMap.b2w_kendotmtabdropdown);
 	}
+
 	public static By getKendoWorkItemTable() {
 		return By.cssSelector(B2WUIMap.b2w_jobstmworkitemgrid);
 	}
+
 	public static By getKendoButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendobutton);
 	}
-	public static By getKendoFooter(){
+
+	public static By getKendoFooter() {
 		return By.cssSelector(B2WUIMap.b2w_kendofooter);
 	}
+
 	public static By getKendoWindow() {
 		return By.cssSelector(B2WUIMap.b2w_kendowindow);
 	}
-	public static By getKendoNumericTextBox(){
+
+	public static By getKendoNumericTextBox() {
 		return By.cssSelector(B2WUIMap.b2w_kendonumerictextbox);
 	}
+
 	public static By getKendoFakeSaveButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendosavebuttonfake);
 	}
+
 	public static By getKendoButtonAdd() {
 		return By.cssSelector(B2WUIMap.b2w_kendobuttonadd);
 	}
+
 	public static By getKendoButtonNext() {
 		return By.cssSelector(B2WUIMap.b2w_kendobuttonnext);
 	}
+
 	public static By getKendoWindowTitle() {
 		return By.cssSelector(B2WUIMap.b2w_kendowindowtitle);
 	}
+
 	public static By getKendoButtonContainer() {
 		return By.cssSelector(B2WUIMap.b2w_kendobuttoncontainer);
 	}
+
 	public static By getKendoDateText() {
 		return By.cssSelector(B2WUIMap.b2w_kendodatetext);
 	}
+
 	public static By getKendoLargeSaveButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendolargesavebutton);
 	}
+
 	public static By getKendoLargeSAVEButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendolargeSavebutton);
 	}
+
 	public static By getKendoCancelButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendocancelbutton);
 	}
+
 	public static By getKendoApproveButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendoapprovebutton);
 	}
+
 	public static By getKendoCompleteButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendocompletebutton);
 	}
+
 	public static By getKendoEditButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendoeditbutton);
 	}
+
 	public static By getKendoDeleteButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendodeletebutton);
 	}
+
 	public static By getKendoConfirmYesButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendoconfirmyesbutton);
 	}
+
 	public static By getKendoConfirmNoButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendoconfirmnobutton);
 	}
+
 	public static By getKendoAddSaveButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendolargeaddsavebutton);
 	}
+
 	public static By getKendoLargeFinishButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendolargefinishbutton);
 	}
+
 	public static By getKendoLargeDoneButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendolargedonebutton);
 	}
+
 	public static By getKendoButtonNew() {
 		return By.cssSelector(B2WUIMap.b2w_kendobuttonnew);
 	}
+
 	public static By getKendoSubmitButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendosubmit);
 	}
+
 	public static By getKendoSelected() {
 		return By.cssSelector(B2WUIMap.b2w_kendoselected);
 	}
+
 	public static By getCompleteSaveButton() {
 		return By.xpath(B2WUIMap.b2w_maintaincompleteworkordercomplete);
 	}
+
 	public static By getCompleteNextButton() {
 		return By.xpath(B2WUIMap.b2w_maintaincompletworkordernext);
 	}
+
 	public static By getKendoIconPlus() {
 		return By.cssSelector(B2WUIMap.b2w_maintainprogramsiconplus);
 	}
+
 	public static By getKendoIconMinus() {
 		return By.cssSelector(B2WUIMap.b2w_maintainprogramsiconminus);
 	}
+
 	public static By getKendoMasterRow() {
 		return By.cssSelector(B2WUIMap.b2w_kendomasterrow);
 	}
@@ -150,8 +193,67 @@ public abstract class KendoUI {
 	public static By getB2WKendoKButton() {
 		return By.cssSelector(B2WUIMap.b2w_kendokbutton);
 	}
-	 public static By getB2WKendoRemoveItemBtn() { 
-		 return By.cssSelector(B2WUIMap.b2w_schedules_removeItemBtn);
+
+	public static By getB2WKendoRemoveItemBtn() {
+		return By.cssSelector(B2WUIMap.b2w_schedules_removeItemBtn);
 	}
 
+	public static By getB2WKendoFooterTemplate() {
+		return By.cssSelector(B2WUIMap.b2w_kendofootertemplate);
+	}
+	
+	public static By getB2WKendoIcon() {
+		return By.cssSelector(B2WUIMap.b2w_kendobuttonicon);
+	}
+	public static By getB2WKendotbodyTag() {
+		return By.tagName(B2WUIMap.b2w_kendotbody);
+	}
+
+	public static By getB2WKendotdTag() {
+		return By.tagName(B2WUIMap.b2w_kendotd);
+	}
+	public static By getB2WKendotrTag() {
+		return By.tagName(B2WUIMap.b2w_kendotr);
+	}
+	public static By getB2WKendoinputTag() {
+		return By.tagName(B2WUIMap.b2w_kendouinputtag);
+	}
+	public static By getB2WKendolabeltag() {
+		return By.tagName(B2WUIMap.b2w_kendolabeltag);
+	}
+	public static By getB2WKendospantag() {
+		return By.tagName(B2WUIMap.b2w_kendospantag);
+	}
+	public static By getB2WKendotextareaTag() {
+		return By.tagName(B2WUIMap.b2w_kendotextareatage);
+	}
+	public static By getB2WKendoiframeTag() {
+		return By.tagName(B2WUIMap.b2w_kendoiframetag);
+	}
+	public static By getB2WKendobodytag() {
+		return By.tagName(B2WUIMap.b2w_kendobodytag);
+	}
+	public static By getB2WKendooptiontag() {
+		return By.tagName(B2WUIMap.b2w_kendooptiontag);
+	}
+	public static By getB2WKendodivtag() {
+		return By.tagName(B2WUIMap.b2w_kendodivtag);
+	}
+	public static By getB2WKendolinktag() {
+		return By.tagName(B2WUIMap.b2w_kendolinktag);
+	}
+	public static By getB2WKendoptag() {
+		return By.tagName(B2WUIMap.b2w_kendoptag);
+	}
+	public static By getB2WKendolitag() {
+		return By.tagName(B2WUIMap.b2w_kendolitage);
+	}
+	public static By getB2WKendotexttag() {
+		return By.tagName(B2WUIMap.b2w_kendotexttage);
+	}
+	public static By getB2WKendostrongtag() {
+		return By.tagName(B2WUIMap.b2w_kendostrongtag);
+	}
+	
+	
 }
